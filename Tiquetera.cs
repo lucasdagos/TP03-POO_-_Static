6f7443ce45093e7c499429182a1c2fb451e5c97f
lucasdagos <168999108+lucasdagos@users.noreply.github.com>
2024-05-13 11:20:45 -0300

    variables tiquetera

---
+++ Tiquetera.cs
@@ -0,0 +1,11 @@
+static class Ticketera
+{
+    private static Dictionary <int, Cliente> DicClientes = new Dictionary<int, Cliente>();
+    private static double
+        OP1 = 45000,
+        OP2 = 60000,
+        OP3 = 30000,
+        OP4 = 100000;
+
+    private static int UltimoIDEntrada = 0;
+}
