b824c83fba2f111be8607cca64116e949e4f91c2
lucasdagos <168999108+lucasdagos@users.noreply.github.com>
2024-05-13 11:22:44 -0300

    modulos tiquetera

--- Tiquetera.cs
+++ Tiquetera.cs
@@ -8,4 +8,65 @@ static class Ticketera
         OP4 = 100000;
 
     private static int UltimoIDEntrada = 0;
+
+        public static int AgregarCliente(Cliente cliente)
+    {
+        int key = Ticketera.DevolverUltimoID();
+        
+        DicClientes.Add(key, cliente);
+        UltimoIDEntrada = key;
+
+        return key++;
+    }
+
+    public static Cliente BuscarCliente(int ID)
+    {
+        Cliente cliente = null;
+        bool condicion = DicClientes.ContainsKey(ID);
+        if (condicion)
+        {
+            cliente = DicClientes[ID];
+        }
+        
+        return cliente;
+    }
+
+    public static bool CambiarEntrada(int ID, int Tipo, int cantidad)
+    {
+        Cliente cliente;
+        bool condicion = DicClientes.ContainsKey(ID);
+        
+        if (condicion != false)
+        {
+            double importe = ObtenerPrecioEntrada(OP1, OP2, OP3, OP4, Tipo, cantidad);
+            cliente = DicClientes[ID];
+            double importeAnterior = ObtenerPrecioEntrada(OP1, OP2, OP3, OP4, cliente.TipoEntrada, cliente.Cantidad);
+            if(importe < importeAnterior)
+            {
+                condicion = false;
+            }
+        }
+
+        return condicion;
+    }
+    static double ObtenerPrecioEntrada(double OP1, double OP2, double OP3, double OP4, int tipo, int cantidad)
+    {
+        double importe = 0;
+        switch (tipo)
+        {
+            case 1:
+                importe = OP1 * cantidad;
+                        break;
+            case 2:
+                importe = OP2 * cantidad;
+                    break;
+            case 3:
+                importe = OP3 * cantidad;
+                    break;
+            case 4:
+                importe = OP4 * cantidad;
+                    break;
+        }
+        return importe;
+    }
 }
